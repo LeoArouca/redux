9d7646d9edab4a34c9596fad61550e2749914149
LeoArouca <leonardo.arouca@gmail.com>
2018-05-26 15:00:20 +0800

    Adds listener and way to remove it

--- index.js
+++ index.js
@@ -6,10 +6,30 @@ function createStore () {
   // 4. Update the state
 
   let state
+  let listeners = []
 
   const getState = () => state
+  // Listen to changes
+  const subscribe = (listener) => {
+    listeners.push(listener);
+    // this will return a function to remove the listener
+    return () => {
+      listeners = listeners.filter((l) => l !== listener)
+    }
+  }
 
   return {
-    getState
+    getState,
+    subscribe,
   }
 }
+// Returns an object with the method getState.
+// When user creates a new store they can acces that
+
+// example usage
+const store = createStore()
+// This would listen to it
+store.subscribe(() => {})
+// to unsubscribe
+const unsubscribe = store.subscribe(() => {})
+// So in the store you need to keep track of the calls
